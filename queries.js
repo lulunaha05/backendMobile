3fcf1e34fd5b5e02b2c06b0a81df05251e98252b
sherlyflorencia <sherlyflorencia01@gmail.com>
2019-12-19 00:29:30 +0700

    7

--- queries.js
+++ queries.js
@@ -317,20 +317,3 @@ module.exports = {
   delete_bookmark,
   show_bookmark
 };
-
-//post tapi bodynya banyak
-// var {param1, param2} = request.body
-//get
-//const _param = request.params.id
-
-// pool.query(
-//   "SELECT * FROM tbl_panti WHERE id_panti = $1",
-//   [id],
-//   [nama],
-//   (error, results) => {
-//     if (error) {
-//       throw error;
-//     }
-//     response.status(200).json(results.rows);
-//   }
-// );
